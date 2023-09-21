051205ae91838b8d14a92eb0ad10b6ce607b00f0
sajibbiswas2 <sajibmagura2021@gmail.com>
2023-09-21 12:02:06 +0600

    switch case

---
+++ switch.js
@@ -0,0 +1,89 @@
+// let day = 1;
+
+// switch (day) {
+//     case 1:
+//         console.log("saturday")
+//         break;
+    
+//     case 2:
+//         console.log("sunday");
+//         break;
+//     case 3:
+//         console.loga("monday")
+//         break;
+//     case 4:
+//         console.log("tuesday")
+//         break;
+//     case 5:
+//         console.log("wednesday")
+//         break;
+//     case 6:
+//         console.log("thursday");
+//         break;
+//     case 7:
+//         console.log("friday")
+//         brack;
+
+//     default:
+//         console.log("invalid day")
+
+
+//    }
+
+
+// switch month dailey
+
+const month = "february";
+switch (month) {
+    
+    case "january":
+        console.log("january")
+        break;
+    case "february":
+        console.log("february")
+        break;
+    
+    case "march":
+        console.log("muarch");
+        break;
+    
+    case "april":
+        console.log("april")
+        break;
+    case "may":
+        console.log("may")
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    
+    case "august":
+        console.log("august");
+        break
+    
+    case "september":
+        console.log("september");
+        break;
+    
+    case "october":
+        console.log("october");
+        break;
+    
+    case "november":
+        console.log("november");
+        break;
+    
+    case "december":
+        console.log("december");
+        break;
+    
+    
+    default:
+        console.log("invalid month")
+    
+}
+
+
