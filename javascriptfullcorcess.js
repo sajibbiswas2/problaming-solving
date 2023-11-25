72a455953b1a6c239e01f807d912bbb81864f5c0
sajibbiswas2 <sajibmagura2021@gmail.com>
2023-11-26 02:33:33 +0600

    arry,function ,multipol array

--- javascriptfullcorcess.js
+++ javascriptfullcorcess.js
@@ -1,4 +1,65 @@
-let a = 5;
-let b = 10;
-var sum = a + b;
-console.log(sum);
+// let a = 5;
+// let b = 10;
+// var sum = a + b;
+// console.log(sum);
+
+//============================
+// var product = 10 * 5;
+// console.log(product);
+
+//==============================
+
+// push 
+// var ourArrray =['sajib','shoage']
+// ourArrray.push(['jamal'])
+// console.log(ourArrray)
+//======================================
+//maltipol array
+//===================
+// var myArray=[['rohim'],['korim']]
+// myArray.push(['jamal'])
+// console.log(myArray)
+//================================
+
+//pop
+//===================
+// var numbr =[4,3,8,5,7,3,5,6,9,8,3]
+// var removarray =numbr.pop();
+// console.log(numbr)
+//=====================
+
+//maltipol arry pop
+//===============
+
+// var myArray=[['rohim'],['korim']]
+// var removmyArray=myArray.pop();
+// console.log(myArray)
+//=============================
+
+//shift
+//===============
+
+// var ourArrray =['sajib','shoage','bagun']
+// ourArrray.shift();
+// console.log(ourArrray)
+
+//===============
+//unshift
+//================
+
+// var ourArrray =['sajib','shoage','bagun']
+// ourArrray.unshift(['morec'])
+// console.log(ourArrray)
+
+
+//=================
+//function
+
+//================
+function mydata (){
+    console.log('this is good boy')
+}
+mydata();
+mydata();
+mydata();
+mydata();
